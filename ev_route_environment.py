f8f4492d120569c1bfd9faabe90ed32f72d7f9fc
Kody <agent.xaber@gmail.com>
2018-12-06 22:46:31 -0700

    Simplified state, started working on q learning algorithm

--- ev_route_environment.py
+++ ev_route_environment.py
@@ -7,6 +7,9 @@ import geopy.distance
 import datetime
 import math
 from enum import Enum
+import random
+from decimal import Decimal
+from decimal import ROUND_HALF_UP
 
 class NavigationAction(Enum):
     driving = 1 
@@ -15,8 +18,11 @@ class NavigationAction(Enum):
 
 class State:
     def __init__(self, time, battery_charge, location):
+        #Omit this first, but this is for the terminal reward later. 
         self.time = time
+        #The amount of charge in the battery at the location. 
         self.battery_charge = battery_charge
+        #Index of the location in the environment list. 
         self.location = location
 
     def get_state(self):
@@ -55,7 +61,7 @@ class EvRouteEnvironment:
 
         print('route points: ', len(self.route_data['route']))
         print('intersections: ',len(self.route_data['intersections']))
-        self.nearest_chargers = {}
+        self.nearest_chargers_dict = {}
 
         print('Getting nearest chargers')
         i = 0
@@ -63,7 +69,7 @@ class EvRouteEnvironment:
             data = self.charger_database.get_nearest_chargers(waypoint)
 
             for row in data:
-                if row['id'] in self.nearest_chargers.keys():
+                if row['id'] in self.nearest_chargers_dict.keys():
                     continue
 
                 addressInfo = co.AddressInfo(accessComments=row['accesscomments'], 
@@ -84,12 +90,26 @@ class EvRouteEnvironment:
                     town=row['town'],
                     distanceFromCurrentWaypoint=row['distancefromcurrentwaypoint'])
 
-                self.nearest_chargers[addressInfo.ID] = [i, addressInfo]
+                self.nearest_chargers_dict[addressInfo.ID] = addressInfo
                 i += 1
 
-        print('Number of chargers along route: ', len(self.nearest_chargers))
-        self.reveresed_chargers = self.reverse(self.nearest_chargers)
-        self.initial_state = State(self.end_time, 0, self.reveresed_chargers[0])
+        print('Number of chargers along route: ', len(self.nearest_chargers_dict))
+        self.chargers = [n[1] for n in self.nearest_chargers_dict.items()]
+        self.locations = list(range(0, len(self.nearest_chargers_dict) - 1, 1))
+        self.states = self.compute_states()
+        #self.reveresed_chargers = self.reverse(self.nearest_chargers)
+
+    def compute_states(self):
+        states = []
+        for stop in range(len(self.nearest_chargers_dict)):
+            for level in range(self.battery_cap):
+                    states.append([stop, level])
+        return states
+
+    def get_random_state(self):
+        b_states = [k for k in range(self.battery_cap)]
+        s = random.sample(b_states,1)[0]
+        return self.states.index([0, s])
 
     def reverse(self, dictionary):
         dict_items = list(dictionary.items())
@@ -99,27 +119,37 @@ class EvRouteEnvironment:
 
     def display_route_in_browser(self):
         print('Displaying route.')
-        self.route_machine.draw_route(self.route_data['route'], [v[1] for v in self.nearest_chargers.values()])
+        self.route_machine.draw_route(self.route_data['route'], [v[1] for v in self.nearest_chargers_dict.values()])
 
-    def get_next_state(self, current_state, action):
-        current_waypoint_index = self.reveresed_chargers.index(current_state.location)
-        current_waypoint_point = [current_state.location.Latitude, current_state.location.Longitude]
+    def get_next_state(self, current_state_index, action):
+        done = False
+        current_state = self.states[current_state_index]
+        #current_waypoint_index = self.reveresed_chargers.index(current_state.location)
+        current_waypoint = self.chargers[current_state[0]]
+        current_waypoint_point = [current_waypoint.Latitude, current_waypoint.Longitude]
 
-        next_waypoint = self.reveresed_chargers[current_waypoint_index + 1]
-        next_waypoint_point = [next_waypoint.Latitude, next_waypoint.Longitude]
+        if self.chargers[current_state[0] + 1]:
+            #next_waypoint = self.reveresed_chargers[current_waypoint_index + 1]
+            next_waypoint = self.chargers[current_state[0] + 1]
+            next_waypoint_point = [next_waypoint.Latitude, next_waypoint.Longitude]
+        else:
+            done = True
+            return current_state, 0, done
 
         distance = self.calculate_distance_between_points(current_waypoint_point, next_waypoint_point)
-        battery_loss = self.calculate_battery_loss(distance)
-        new_battery = current_state.battery_charge + battery_loss
-        new_time = self.calculate_time_to_waypoint(current_state.time, distance)
-        reward = self.calculate_reward(action, new_battery, battery_loss)
-        return State(new_time, new_battery, next_waypoint), reward
+        new_battery = self.calculate_battery_loss(distance, action, current_state[1])
+        #new_time = self.calculate_time_to_waypoint(current_state.time, distance)
+        reward = self.calculate_reward(action, new_battery, new_battery - current_state[1])
+        next_state = [current_state[0] + 1, new_battery]
+        return next_state, reward, done
     
     def calculate_reward(self, action, battery_state, battery_charge_amount):
         if action == NavigationAction.driving:
             return (-1/(6 * math.pow(battery_state + 4, 3)+ 1))
         else:
 	    #multiply the amount charged by $1.20 (average price of electricity
+            if battery_state > 10:
+                return -100
             return battery_charge_amount * 1.20
 
     #def calculate_time_to_waypoint(self, time, current_waypoint, next_waypoint):
@@ -127,8 +157,14 @@ class EvRouteEnvironment:
         time_to_travel = (distance /  45) *  60
         return time + datetime.timedelta(minutes = time_to_travel)
     
-    def calculate_battery_loss(self, distance):
-        return distance / 5
+    def calculate_battery_loss(self, distance, action, current_battery):
+        if action == NavigationAction.driving:
+            return int(Decimal(str(current_battery - (distance / 5))).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
+        elif action == NavigationAction.charging & current_battery < 80:
+            return current_battery + (10)
+        else:
+            return int(Decimal(str(current_battery + ((1/3.2) * math.pow(current_battery - 80,2) + 40))).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
+            
 
     def calculate_distance_between_points(self, point_1, point_2):
         return geopy.distance.distance(point_1, point_2).miles
--- ev_route_optimizer.py
+++ ev_route_optimizer.py
@@ -1,15 +1,64 @@
 import ev_route_environment as ev_route_environment
 import datetime
 from datetime import datetime as dt
+import numpy as np
+import random
 #create a new environment
+
 time = dt.strptime('16:00', '%H:%M')
-env = ev_route_environment.EvRouteEnvironment(time,150, route_from_file=True)
+env = ev_route_environment.EvRouteEnvironment(time,100, 
+    startLocation=['-111.8338', '41.7370'], endLocation=['-112.024867','41.289583'],
+    charger_radius=3,route_from_file=False)
+
+print([state for state in env.states])
+print('number of states ', len(env.states))
+
 
 #create the action space
 action_space = ev_route_environment.NavigationAction
 
-#test getting the next action.
+# #test getting the next action.
 action = action_space.driving
-start_state = env.initial_state
-next_state,reward = env.get_next_state(start_state, action)
-print(next_state.time, next_state.battery_charge, next_state.location.Title, reward)
+start_state = env.get_random_state()
+next_state,reward, done = env.get_next_state(start_state, action)
+
+# Hyperparameters
+# alpha = 0.1
+# gamma = 0.6
+# epsilon = 0.1
+
+# # For plotting metrics
+# all_epochs = []
+# all_penalties = []
+
+# for i in range(1, 100001):
+#     state = env.reset()
+
+#     epochs, penalties, reward, = 0, 0, 0
+#     done = False
+    
+#     while not done:
+#         if random.uniform(0, 1) < epsilon:
+#             action = env.action_space.sample() # Explore action space
+#         else:
+#             action = np.argmax(q_table[state]) # Exploit learned values
+
+#         next_state, reward, done, info = env.step(action) 
+        
+#         old_value = q_table[state, action]
+#         next_max = np.max(q_table[next_state])
+        
+#         new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
+#         q_table[state, action] = new_value
+
+#         if reward == -10:
+#             penalties += 1
+
+#         state = next_state
+#         epochs += 1
+        
+#     if i % 100 == 0:
+#         clear_output(wait=True)
+#         print(f"Episode: {i}")
+
+# print("Training finished.\n")
